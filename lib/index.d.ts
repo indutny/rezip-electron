cb0ad95051008b9d1dac9f6c8024bb548cbc8a7e
Fedor Indutny <238531+indutny@users.noreply.github.com>
2024-02-23 11:04:58 -0800

    fix: types

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -4,4 +4,18 @@ export function optimize(options: {
   blockMapPath?: string;
 }): Promise<void>;
 
-export function compare(oldFile: string, newFile: string);
+export function compare(
+  oldFile: string,
+  newFile: string,
+): Promise<{
+  downloadSize: number;
+  modifiedFiles: Array<{
+    resolvedPath: string;
+    originalPath: string;
+    subPath?: string;
+    resolvedOffset: number;
+    fileOffset: number;
+    zipOffset: number;
+    size: number;
+  }>;
+}>;
